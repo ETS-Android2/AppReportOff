2293f1e3799f65950253ba1b02fe14942cbacd90
alfser <j.janilson12@gmail.com>
2021-07-10 01:27:00 -0300

    Genereate Pdf file and shere it

--- CelulaReport/app/src/main/java/com/example/celulareport/ui/fragment/ReportDetailsFragment.java
+++ CelulaReport/app/src/main/java/com/example/celulareport/ui/fragment/ReportDetailsFragment.java
@@ -1,5 +1,9 @@
 package com.example.celulareport.ui.fragment;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,6 +13,11 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.os.Environment;
+import android.os.Parcelable;
+import android.os.StrictMode;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -16,6 +25,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +35,13 @@ import com.example.celulareport.R;
 import com.example.celulareport.db.model.ReportEntity;
 import com.example.celulareport.viewmodel.ReportsViewModel;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Objects;
 
 public class ReportDetailsFragment extends Fragment {
 
@@ -39,7 +58,7 @@ public class ReportDetailsFragment extends Fragment {
     TextView mVisitantesText;
     TextView mOfertaText;
     TextView mEstudoText;
-    TextView mCommitsText;
+    MultiAutoCompleteTextView mCommitsText;
 
     //Toolbar
     Toolbar mToolbar;
@@ -47,6 +66,9 @@ public class ReportDetailsFragment extends Fragment {
 
     //Id of report
     private long mReportID;
+    private ReportEntity reportEntity;
+
+    private View viewRoot;
 
     public ReportDetailsFragment() {
         // Required empty public constructor
@@ -66,6 +88,12 @@ public class ReportDetailsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
+        //Just for propose test
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        builder.detectFileUriExposure();
+
         if(getArguments() != null){
             mReportID = getArguments().getLong(REPORT_ID);
         }else {
@@ -79,7 +107,6 @@ public class ReportDetailsFragment extends Fragment {
         setHasOptionsMenu(true);
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_report_details, container, false);
-
         return v;
     }
 
@@ -95,6 +122,8 @@ public class ReportDetailsFragment extends Fragment {
 
         //Setup toolbar
         SetupToolbar(view);
+
+        viewRoot = view;
     }
 
     public void  getViewObjects(View v){
@@ -122,6 +151,7 @@ public class ReportDetailsFragment extends Fragment {
 
 
     public void addingReportDetails(ReportEntity mReport){
+        reportEntity = mReport;
         String celula = mReport.getNomeCelula();
         mToolbar.setTitle(celula);
 
@@ -134,6 +164,8 @@ public class ReportDetailsFragment extends Fragment {
         mOfertaText.setText(mReport.getOferta());
         mEstudoText.setText(mReport.getEstudo());
         mCommitsText.setText(mReport.getComentarios());
+
+        Log.i("ReportDetailsFragment", "Commit Text: "+mReport.getComentarios());
     }
 
     private void SetupToolbar(View v){
@@ -170,11 +202,103 @@ public class ReportDetailsFragment extends Fragment {
                 return true;
 
             case R.id.ic_details_share:
+                File pdfFile = createReportPDF();
+                final Uri uri = Uri.fromFile(pdfFile);
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "Relatório de"+reportEntity.getDataToShow());
+                sendIntent.putExtra(Intent.EXTRA_STREAM, uri);
+                sendIntent.putExtra(Intent.EXTRA_TITLE, "Relatório de Célula");
+                sendIntent.setType("application/pdf");
+
+                Intent share = Intent.createChooser(sendIntent, getString(R.string.sharing_title));
+                startActivity(share);
+
                 Toast.makeText(getContext(), "Shared icon clicked", Toast.LENGTH_SHORT).show();
                 return true;
         }
 
-
         return super.onOptionsItemSelected(item);
     }
+
+    @NonNull
+    private File createReportPDF(){
+
+        // create a new document
+        PdfDocument document = new PdfDocument();
+
+        // create a page description
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(900, 1424, 1).create();
+
+        // start a page
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        // draw something on the page
+        //View content = viewRoot.findViewById(R.id.layout_report_details);
+
+        View content = View.inflate(getContext(), R.layout.report_page, null);
+        TextView celula = content.findViewById(R.id.txt_celula);
+        celula.setText(reportEntity.getNomeCelula());
+
+        TextView lider = content.findViewById(R.id.txt_lider);
+        lider.setText(reportEntity.getNomeLider());
+
+        TextView colider = content.findViewById(R.id.txt_colider);
+        colider.setText(reportEntity.getNomeColider());
+
+        TextView anfitriao = content.findViewById(R.id.txt_anfitriao);
+        anfitriao.setText(reportEntity.getNomeAnfitriao());
+
+        TextView membros = content.findViewById(R.id.txt_membros);
+        membros.setText(reportEntity.getNumMembros());
+
+        TextView visitantes = content.findViewById(R.id.txt_visitantes);
+        visitantes.setText(reportEntity.getNumVisitantes());
+
+        TextView data = content.findViewById(R.id.txt_data);
+        data.setText(reportEntity.getDataToShow());
+
+        TextView versiculo = content.findViewById(R.id.txt_estudo);
+        versiculo.setText(reportEntity.getEstudo());
+
+        TextView oferta = content.findViewById(R.id.txt_oferta);
+        oferta.setText(reportEntity.getOferta());
+
+        TextView comentarios = content.findViewById(R.id.txt_commits);
+        comentarios.setText(reportEntity.getComentarios());
+
+
+        //draw according to page size
+        content.measure(900, 1424);
+        content.layout(0, 0, 900, 1424);
+        content.draw(page.getCanvas());
+
+        // finish the page
+        document.finishPage(page);
+
+        //create a file do write pdf
+        File path  = Objects.requireNonNull(requireContext()).getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
+        //String path = "/sdcard/"+"report-"+reportEntity.getDataReuniao()+".pdf";
+        String fileName = "report-"+reportEntity.getDataReuniao()+".pdf";
+        File file = new File(path, fileName);
+
+        try {
+            OutputStream outputStream = new FileOutputStream(file);
+            document.writeTo(outputStream);
+            // write the document content
+            Toast.makeText(getActivity(), "PDF file generated successfully. Local: "+file.getPath(), Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        // close the document
+        document.close();
+
+        return file;
+    }
+
+
 }
